695c608d9a846e6c19e9170bb82627d410df9e2d
Hope Ngerebara <hope.ngerebara@andela.com>
2018-09-21 12:09:36 +0100

    set up root module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hopeaz Random Phone Number Generator';
 }
+
+
