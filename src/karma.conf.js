bfb44ab2947e9a2a7bab84db1fc911486bc9bf8f
Hope Ngerebara <hope.ngerebara@andela.com>
2018-09-23 17:31:07 +0100

    add coveralls

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -13,23 +10,29 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-coverage')
     ],
-    client: {
+    client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
     preprocessors: { 'dist/app/**/!(*spec).js': ['coverage'] },
+    mime: {
+      'text/x-typescript': ['ts','tsx']
+    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    coverageReporter: {
-      dir : 'coverage/',
-        reporters: [
-          { type: 'html' },
-          { type: 'lcov' }
-        ]
-    },    
-    reporters: ['progress', 'kjhtml'],
+    angularCli: {
+      config: './angular.json',
+      codeCoverage: 'coverage',
+      environment: 'dev'
+    },
+    reporters: config.angularCli && config.angularCli.codeCoverage
+              ? ['progress', 'coverage']
+              : ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
